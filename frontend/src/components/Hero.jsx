60057816be2773fe0ac97b8f825158f119dcd298
Veselin Metodiev <v.metodiev1718@gmail.com>
2024-01-29 10:51:01 +0200

    update hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,44 @@
-import { Container, Card, Button } from "react-bootstrap";
+import { Form, Container, Card, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useCreateCharacterMutation } from "../slices/characterSlice";
 
 const Hero = () => {
+  const [name, setName] = useState("");
+
+  const [createCharacter, { isLoading: isCreatingCharacter }] =
+    useCreateCharacterMutation();
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleCreateCharacter = async (e) => {
+    e.preventDefault();
+    try {
+      const newCharacterData = {
+        name: name,
+        gold: 100,
+        experience: 0,
+        fame: 0,
+        attributes: {
+          strength: 10,
+          agility: 10,
+          constitution: 10,
+          intelligence: 10,
+        },
+      };
+
+      const result = await createCharacter(newCharacterData);
+      navigate("/tanoth");
+      if (result.data) {
+        setSelectedCharacterId(result.data._id);
+      }
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
   return (
     <div className=" py-5">
       <Container className="d-flex justify-content-center">
@@ -11,6 +49,25 @@ const Hero = () => {
             an HTTP-Only cookie. It also uses Redux Toolkit and the React
             Bootstrap library
           </p>
+          <Form onSubmit={handleCreateCharacter}>
+            <Form.Group className="my-2" controlId="email">
+              <Form.Label>Your unique character's name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button
+              disabled={isCreatingCharacter}
+              type="submit"
+              variant="primary"
+              className="mt-3"
+            >
+              Sign In
+            </Button>
+          </Form>
           <div className="d-flex">
             <Button variant="primary" href="/tanoth" className="me-3">
               Tanoth
